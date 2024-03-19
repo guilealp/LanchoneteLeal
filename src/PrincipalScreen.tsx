b726fe440fd749d7015fb035994d22af36ae4df6
guilealp <guilherme.3110@hotmail.com>
2024-03-19 15:49:43 -0300

    aula 19/03

---
+++ src/PrincipalScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import CardapioScreen from './cardapioScreen';
+import { useLinkTo } from '@react-navigation/native';
+
+function PrincipalScreen(): React.JSX.Element {
+
+
+return(
+    <View style={styles.container}>
+        <TouchableOpacity >
+            <Image source={require('./assents/images/hamburguer.png')} style={styles.img} />
+        </TouchableOpacity>
+    </View>
+);
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#f0971c',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    img:{
+        
+    },
+
+});
+
+export default PrincipalScreen;
