f0bbf15a4ed27895ede2f58fd08f9f6fec05a795
guilealp <guilherme.3110@hotmail.com>
2024-04-02 15:35:51 -0300

    aula 02/04

--- App.tsx
+++ App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { View } from 'react-native';
 import PrincipalScreen from './src/PrincipalScreen';
 import CardapioScreen from './src/CardapioScreen';
+import LoginScreen from './src/LoginScreen';
+import CadastroProduto from './src/screens/CadastroProdutos';
 
 
 
 function App(): JSX.Element {
 
   return (
-  <CardapioScreen/>
+  //<CardapioScreen/>
+  //<LoginScreen/>
+  <CadastroProduto/>
   )
 }
 
---
+++ src/interface/ProdutoInterface.ts
@@ -0,0 +1,7 @@
+interface Produto{
+    id:number;
+    nome:string;
+    ingredientes:string,
+    preco:Number;
+    imagem:any;
+}
